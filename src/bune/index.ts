0a850b7e2e0e13cfca619d09b1b8efd28aa08527
jwu <jianping.wu@eisgroup.com>
2024-04-03 18:18:20 +0800

    add test bun

--- src/bune/index.ts
+++ src/bune/index.ts
@@ -1,30 +1,29 @@
-import {argv,file} from 'bun'
+import {argv, file, $} from 'bun'
 import path from 'path'
+import pc from 'picocolors'
+
 const url = argv[2]
 if (URL.canParse(url)) {
     try {
         const res = await fetch(url)
         if (res.ok) {
-            eval(await res.text())
+            await $`bun -e ${await res.text()}`
         } else {
-            console.log(`error: Module not found ${url}.`)
+            console.error(pc.red('error: '), `Module not found ${url}.`)
         }
-    } catch (e) {
-        console.error(e)
+    } catch (e: any) {
+        console.error(pc.red(e))
     }
 } else {
     try {
-        const currentPath = path.join(process.cwd(), url)
+        const currentPath = path.join(process.cwd(), url ?? '')
         const local_file = file(currentPath)
-        if(await local_file.exists()){
-            console.log(await local_file.text(),44)
-            eval(await local_file.text())
-        }else {
-            console.log(`error: Module not found ${currentPath}.`)
+        if (await local_file.exists()) {
+            await $`bun run ${currentPath}`
+        } else {
+            console.error(pc.red('error: '), `Module not found ${currentPath}.`)
         }
-    }catch (e) {
-        console.error(e,12122)
+    } catch (e: any) {
+        console.error(pc.red(e))
     }
 }
-
-
