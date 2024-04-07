643c9e0e8e49998ff2e20b5fce384e89f798f182
jwu <jianping.wu@eisgroup.com>
2024-04-07 13:51:13 +0800

    add test bun

--- src/bune/index.ts
+++ src/bune/index.ts
@@ -1,13 +1,31 @@
-import {argv, file, $} from 'bun'
+import {$, argv, file, write} from 'bun'
+import os from 'os'
 import path from 'path'
 import pc from 'picocolors'
 
 const url = argv[2]
+
+const getFileName = (url: string): string => path.basename(url)
+
+const createTempFile = async (tempFilePath: string, content: string) => {
+    await write(tempFilePath, content)
+}
+
+const getTempFilePath = (url: string) => path.join(os.tmpdir(), getFileName(url))
+
 if (URL.canParse(url)) {
     try {
         const res = await fetch(url)
         if (res.ok) {
-            await $`bun -e ${await res.text()}`
+            if (url.endsWith('.sh')) {
+                const text = await res.text()
+                const tempFilePath = getTempFilePath(url)
+                await createTempFile(tempFilePath, text)
+                //Variables are also escaped to prevent command injection.
+                await $`bun run ${tempFilePath}`
+            } else {
+                await $`bun -e ${await res.text()}`
+            }
         } else {
             console.error(pc.red('error: '), `Module not found ${url}.`)
         }
